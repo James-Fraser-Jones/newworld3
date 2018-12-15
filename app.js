e32c9652a4168b42f7ce0b808ebcfa53fd48c14b
smudgecat123 <sha.dow@hotmail.co.uk>
2018-12-15 15:54:17 +0000

    huge cleanup

--- app.js
+++ app.js
@@ -4,93 +4,32 @@
 //Global variables
 
 const fs = require('fs');
-const Database = require('better-sqlite3');
-
-const helperDB = require('./helperDB');
-const newworldDB = require('./newworldDB');
-
-var db = null; //global database reference
+const NewworldDB = require('./newworldDB');
+const db = new NewworldDB();
 
 //==============================================================================
-//DB creation (using db and helper modules)
+//DB Functions
 
 function createMyDB(){
-  let dbName = "auctionDB";
-
-  let dbObj = helperDB.create(dbName);
-
-  let permission = helperDB.addTable(dbObj, "Permission", false);
-  helperDB.addField(permission, "Name", helperDB.type.TEXT, false, false, null, null, null);
-
-  let bod = helperDB.addTable(dbObj, "Bod", true);
-  helperDB.addField(bod, "PermissionID", helperDB.type.INTEGER, false, false, null, null, "Permission");
-  helperDB.addField(bod, "FirstName", helperDB.type.TEXT, false, false, null, null, null);
-  helperDB.addField(bod, "LastName", helperDB.type.TEXT, false, false, null, null, null);
-  helperDB.addField(bod, "ContactNum", helperDB.type.TEXT, false, false, null, null, null);
-  helperDB.addField(bod, "Email", helperDB.type.TEXT, false, false, null, null, null);
-  helperDB.addField(bod, "Address", helperDB.type.TEXT, true, false, null, null, null);
-
-  let item = helperDB.addTable(dbObj, "Item", true);
-  helperDB.addField(item, "OwnerID", helperDB.type.INTEGER, false, false, null, null, "Bod");
-  helperDB.addField(item, "Name", helperDB.type.TEXT, false, false, null, null, null);
-  helperDB.addField(item, "Desc", helperDB.type.TEXT, false, false, null, null, null);
-
-  let auction = helperDB.addTable(dbObj, "Auction", true);
-  helperDB.addField(auction, "Address", helperDB.type.TEXT, false, false, null, null, null);
-  helperDB.addField(auction, "Start", helperDB.type.TEXT, false, false, null, null, null);
-  helperDB.addField(auction, "End", helperDB.type.TEXT, false, false, null, null, null);
-
-  let auctionItem = helperDB.addTable(dbObj, "AuctionItem", true);
-  helperDB.addField(auctionItem, "AuctionID", helperDB.type.INTEGER, false, false, null, null, "Auction");
-  helperDB.addField(auctionItem, "ItemID", helperDB.type.INTEGER, false, false, null, null, "Item");
-  helperDB.addField(auctionItem, "TransactionID", helperDB.type.INTEGER, false, false, null, null, "Transaction");
-  helperDB.addField(auctionItem, "AskingPrice", helperDB.type.INTEGER, false, false, null, null, null);
-  helperDB.addField(auctionItem, "ReservePrice", helperDB.type.INTEGER, true, false, null, null, null);
-  helperDB.addField(auctionItem, "PresaleBidPrice", helperDB.type.INTEGER, true, false, null, null, null);
-
-  let transaction = helperDB.addTable(dbObj, "Transaction", true);
-  helperDB.addField(transaction, "BuyerID", helperDB.type.INTEGER, false, false, null, null, "Bod");
-  helperDB.addField(transaction, "SellerID", helperDB.type.INTEGER, false, false, null, null, "Bod");
-  helperDB.addField(transaction, "ItemID", helperDB.type.INTEGER, false, false, null, null, "Item");
-  helperDB.addField(transaction, "Price", helperDB.type.INTEGER, false, false, null, null, null);
-  helperDB.addField(transaction, "Happened", helperDB.type.TEXT, false, false, null, null, null);
-  helperDB.addField(transaction, "BeenPaid", helperDB.type.INTEGER, false, false, null, null, null);
-
-  let initInsert =
-  [{tableName:"Permission", fieldNames:["PermissionID", "Name"], values:[1, "Admin"]},
-   {tableName:"Permission", fieldNames:["PermissionID", "Name"], values:[2, "Manager"]},
-   {tableName:"Permission", fieldNames:["PermissionID", "Name"], values:[3, "Employee"]},
-   {tableName:"Permission", fieldNames:["PermissionID", "Name"], values:[4, "Customer"]},
-   {tableName:"Bod", fieldNames:["PermissionID", "FirstName", "LastName", "ContactNum", "Email"], values:[1, "Admin", "Admin", "", ""]}];
-
-  newworldDB.create(db, dbName, dbObj, initInsert);
+  //create tables
+  let tableObjs = JSON.parse(fs.readFileSync('database/auctionTables.json', 'utf8'));
+  tableObjs.forEach(db.createTable, db); // <-- second parameter specifies what "this." variable should reference
+  //insert records
+  let insertObjs = JSON.parse(fs.readFileSync('database/auctionInserts.json', 'utf8'));
+  insertObjs.forEach(db.insertRecord, db); // <-- second parameter specifies what "this." variable should reference
 }
-
-createMyDB();
 
 //==============================================================================
 //DB Testing
 
-// //test cell updating
-// let exampleUpdates =
-// [{tableName:"Bod", fieldName:"Email", pkID:1, value:"Mr Johnson"},
-//  {tableName:"Bid", fieldName:"Address", pkID:1, value:55}];
-// console.log("\nUpdate Test:\n");
-// console.log(helperDB.update(db, exampleUpdates));
-//
-// //test row inserting
-// let exampleInserts =
-// [{tableName:"Bod", fieldNames:["PermissionID", "FirstName", "LastName", "ContactNum", "Email", "Address"], values:[1, "Alex", "Baby", "07940234567", "donkey@home.co.uk", "My house"]},
-//  {tableName:"Bod", fieldNames:["BermissionID", "FirstName", "LastName", "ContactNum", "Email", "Address"], values:[2, "Johnny", "Rotten", "666", "dunkey@home.co.uk", "Peach's Castle"]}];
-// console.log("\n\nInsert Test:\n");
-// console.log(helperDB.insert(db, exampleInserts));
-//
-// //test row deletion
-// let exampleDeletes =
-// [{tableName:"Permission", pkID:3},
-//  {tableName:"Parmission", pkID:4}];
-// console.log("\n\nDelete Test:\n");
-// console.log(helperDB.delete(db, exampleDeletes));
+//reset db
+db.openDatabase("auctionDB");
+db.deleteDatabase();
+
+//create tables
+db.openDatabase("auctionDB");
+createMyDB();
+console.log(db.db.open); //db isn't private :/
 
 //==============================================================================
 //Notes
@@ -107,31 +46,4 @@ Money (Integer)
 /*
 Atom shortcut for collapse all when viewing JSON:
 CTRL + ALT + SHIFT + '['
-*/
-
-/*
-New helperDB API:
-
- CreateTable :: DB -> (String, Bool, [(String, Int, Bool, Bool, a, String, String)]) -> IO Maybe Error
-InsertRecord :: DB -> (String, [(String, a)])                                        -> IO (String, [(String, a)], Either Error Int)
-  UpdateCell :: DB -> (String, String, Int, a)                                       -> IO (String, String, Int, a, Either Error a)
-DeleteRecord :: DB -> (String, Int)                                                  -> IO (String, Int, Maybe Error)
-
-These functions use the objects directly to represent these tuples.
-Then just map these functions over Tables, URecords, Cells, DRecords objects, respectively, no need to create new functions for this.
-
-Forget object making functions. Just have a static "Tables" JSON file for your database and access it via the filesystem when you want to
-map "CreateTable" over it.
-
-Print errors created by CreateTable to the console, errors from other functions will be captured and dealt with client side.
-
-////////////////////////////////////////////////////////////////////////////////////
-
-Other functions:
-
-Generic file deleting function which can be used to delete database files.
-OpenDatabase database wrapper function.
-InitDatabase database function which takes a newly opened database and (attempts to) adds all the tables from the Tables.JSON file to it.
-
-Might not need a CloseDatabase database wrapper funciton. (Need to test how this interacts with database file getting deleted etc..)
 */
--- newworldDB.js
+++ newworldDB.js
@@ -1,71 +1,184 @@
 'use strict';
 
 //==============================================================================
-//Module
-
-const newworldDB = {
-  open: openDB,
-  close: closeDB,
-  delete: deleteDB,
-  create: createDB
-}
-
-module.exports = newworldDB
-
-//==============================================================================
-//Global variables
+//Dependencies and Globals
 
 const fs = require('fs');
 const Database = require('better-sqlite3');
 
-const helperDB = require('./helperDB');
-
 //==============================================================================
-//Exposed methods
+//Class Definition
 
-function openDB(db, dbName){
-  db = new Database(`database/${dbName}.db`);
-}
+class NewworldDB {
 
-function closeDB(db){
-  if (db && db.open){
-    db.close();
-    db = undefined;
-  }
-}
+  //====================================
+  //Constructor
 
-function deleteDB(db, dbName){
-  closeDB(db);
-  try{
-    fs.unlinkSync(`database/${dbName}.db`);
+  //declares sqlite-better variable
+  constructor(){
+    let db;
   }
-  catch(err){
-    console.log(err);
-  }
-  try{
-    fs.unlinkSync(`database/${dbName}.json`);
-  }
-  catch(err){
-    console.log(err);
-  }
-}
 
-function createDB(db, dbName, dbObj, insertObj){
-  deleteDB(db, dbName);
-  openDB(db, dbName);
+  //====================================
+  //DB Creation, Connection and Deletion
 
-  let gen = helperDB.generate(db, dbObj);
-  if (gen.success){
-    fs.writeFile(`database/${dbName}.json`, JSON.stringify(dbObj, null, 2), function(err){
-      if (err){
-        console.log(err);
+  //opens database, or creates new one if file is not present
+  openDatabase(fileName){
+    try{
+      this.db = new Database(`database/${fileName}.db`);
+    }
+    catch (err){
+      console.log(err);
+    }
+  }
+
+  //attempts to close current database connection
+  closeDatabase(){
+    try{
+      this.db.close();
+    }
+    catch (err){
+      console.log(err);
+    }
+  }
+
+  //will attempt to close and subsequently delete the previously opened database file
+  deleteDatabase(){
+    try{
+      let name = this.db.name;
+      this.closeDatabase();
+      fs.unlinkSync(name);
+    }
+    catch(err){
+      console.log(err);
+    }
+  }
+
+  //====================================
+  //DB Manipulation
+
+  //creates database table specified by tableObj, otherwise prints error message to console
+  createTable(tableObj){
+    try{
+      //getting all SQL
+      let sqlPK = `  '${tableObj.name}ID' INTEGER NOT NULL PRIMARY KEY ${tableObj.autoinc ? "AUTOINCREMENT " : ""}UNIQUE`;
+
+      let sqlFields = tableObj.fields.map(fieldObj => {
+        let words = [];
+        words.push(`  '${fieldObj.name}'`);
+        words.push(this.getSQLiteType(fieldObj.type));
+        if (!fieldObj.nullable){ words.push("NOT NULL") };
+        if (fieldObj.default){ words.push(`DEFAULT ${this.wrapString(fieldObj.default)}`) };
+        if (fieldObj.check){ words.push(`CHECK (${fieldObj.check})`) };
+        if (fieldObj.unique){ words.push("UNIQUE") };
+        return words.join(" ");
+      });
+
+      let sqlForeigns = tableObj.fields.map(fieldObj => {
+        return fieldObj.foreign ? `  FOREIGN KEY ('${fieldObj.name}') REFERENCES '${fieldObj.foreign}'(${fieldObj.foreign}ID)` : "";
+      }).filter(str => str !== "");
+
+      //joining SQL together
+      let lines = sqlFields.concat(sqlForeigns);
+      lines.unshift(sqlPK);
+
+      //getting final SQL string
+      let sql = `CREATE TABLE '${tableObj.name}' (\n${lines.join(",\n")}\n);`;
+
+      //executing sql string
+      this.db.exec(sql);
+    }
+    catch (err){
+      console.log(err);
+    }
+  }
+
+  //adds {success:true, response:lastInsertedID} or {success:false, response:errorMessage} as result to insertObj
+  insertRecord(insertObj){
+    try{
+      let sql = `INSERT INTO ${insertObj.tableName}(${insertObj.fieldNames.join(",")}) VALUES (${insertObj.values.map(() => "?").join(",")})`
+      let preparedInsert = this.db.prepare(sql);
+      let lastInsertedID = preparedInsert.run(insertObj.values).lastInsertRowid;
+      insertObj.result = {success: true, response: lastInsertedID};
+    }
+    catch (err){
+      insertObj.result = {success: false, response: err};
+    }
+  }
+
+  //adds {success:true, response:newValue} or {success:false, response:errorMessage} as result to updateObj
+  updateCell(updateObj){
+    try{
+      //update
+      let sql = `UPDATE ${updateObj.tableName} SET ${updateObj.fieldName} = ? WHERE ${updateObj.tableName}ID = ?`
+      let preparedUpdate = this.db.prepare(sql);
+      preparedUpdate.run(updateObj.value, updateObj.pkID);
+      //query
+      sql = `SELECT ${updateObj.fieldName} FROM ${updateObj.tableName} WHERE ${updateObj.tableName}ID = ?`
+      let preparedQuery = this.db.prepare(sql);
+      let newValue = preparedQuery.get(updateObj.pkID)[updateObj.fieldName];
+      //return success
+      updateObj.result = {success: true, response: newValue};
+    }
+    catch (err){
+      updateObj.result = {success: false, response: err};
+    }
+  }
+
+  //adds {success:true} or {success:false, response:errorMessage} as result to deleteObj
+  deleteRecord(deleteObj){
+    try{
+      let sql = `DELETE FROM ${deleteObj.tableName} WHERE ${deleteObj.tableName}ID = ?`;
+      let preparedDelete = this.db.prepare(sql);
+      let numberOfChangedRows = preparedDelete.run(deleteObj.pkID).changes;
+      if (numberOfChangedRows === 1){
+        deleteObj.result = {success: true};
       }
       else{
-        console.log(helperDB.insert(db, insertObj));
+        deleteObj.result = {success: false, response: `Error: numberOfChangedRows = ${numberOfChangedRows}`}
       }
-    });
+    }
+    catch (err){
+      deleteObj.result = {success: false, response: err};
+    }
   }
-  else{
-    console.log(gen.response);
+
+  //====================================
+  //Helper functions
+
+  //get name of SQLite type
+  getSQLiteType(num){
+    switch(num){
+      case 1:
+        return "INTEGER"
+        break;
+      case 2:
+        return "TEXT"
+        break;
+      case 3:
+        return "BLOB"
+        break;
+      case 4:
+        return "REAL"
+        break;
+      case 5:
+        return "NUMERIC"
+        break;
+      default:
+        return null;
+    }
+  }
+
+  //wrap string values in single quotes, not necessary when utilizing prepared statements for value injection
+  wrapString(value){
+    if ((typeof value) === "string"){
+      return `'${value}'`;
+    }
+    return value;
   }
 }
+
+//==============================================================================
+//Module
+
+module.exports = NewworldDB;
